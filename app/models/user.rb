25d8a2861b5059acbb09bdf07c8cde0a45a37c34
Britt Tarien <britt.tarien@gmail.com>
2020-12-15 10:28:12 +0100

    navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   has_one_attached :avatar
+  has_one :player
 
   def set_default_avatar
-    unless avatar.attached?
-      avatar.attach(io: URI.open(LetterAvatar.generate(name, 600)), filename: "#{name}.png", content_type: 'image/png')
-    end
+    return if avatar.attached?
+
+    avatar.attach(io: URI.open(LetterAvatar.generate(name, 600)), filename: "#{name}.png", content_type: 'image/png')
   end
 end
