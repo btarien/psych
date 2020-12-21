0c70d22b5c981d9869c3a96037b230196517bfbd
Britt Tarien <britt.tarien@gmail.com>
2020-12-21 16:57:50 +0100

    validate cant vote for own answer

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,14 +9,19 @@ class VotesController < ApplicationController
   def create
     @player = Player.find_by(user: current_user)
     @answer = Answer.find(params[:answer_id])
-    Vote.create!(answer: @answer, player: @player)
-    @answer.player.increment!(:points)
-    if @answer.question.votes.count == @answer.game.players.count
-      GameChannel.broadcast_to(
-        @answer.game,
-        { action: "show votes" }
-      )
+    @vote = Vote.new(answer: @answer, player: @player)
+    @answers = @answer.question.answers
+    if @vote.save
+      @answer.player.increment!(:points)
+      if @answer.question.votes.count == @answer.game.players.count
+        GameChannel.broadcast_to(
+          @answer.game,
+          { action: "show votes" }
+        )
+      end
+      redirect_to answer_votes_path(@answer)
+    else
+      render 'answers/index'
     end
-    redirect_to answer_votes_path(@answer)
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,9 @@ class Vote < ApplicationRecord
   belongs_to :answer
   belongs_to :player
   has_one :question, through: :answer
+  validate :not_own
+
+  def not_own
+    errors.add(:player, "You can't vote for your own answer") if answer.player == player
+  end
 end
